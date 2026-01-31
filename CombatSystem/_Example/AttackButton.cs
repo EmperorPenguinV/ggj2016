82dcc251933949f00dd86ab6d234bef898934e31
Ruceus <ruceus@gmail.com>
2026-01-31 14:45:03 +0100

    refactor: AttackButton uses IDamagable to deal damage

--- CombatSystem/_Example/AttackButton.cs
+++ CombatSystem/_Example/AttackButton.cs
@@ -3,18 +3,18 @@ using Godot;
 public partial class AttackButton : Button
 {
 	[Export] public AttackData AttackData;
-	[Export] public NodePath EnemyPath;
+	[Export] public NodePath Damagable;
 
-	private Enemy _enemy;
+	private IDamageable _damagable;
 
 	public override void _Ready()
 	{
-		_enemy = GetNode<Enemy>(EnemyPath);
+		_damagable = GetNode<IDamageable>(Damagable);
 		Pressed += OnPressed;
 	}
 
 	private void OnPressed()
 	{
-		_enemy.TakeDamage(AttackData);
+		_damagable.TakeDamage(AttackData);
 	}
 }
