2a7c7fc4385f268f2dc2566de5a270968f5e3d59
Raven <ravioli95@hotmail.de>
2026-01-31 18:40:51 +0100

    removed ids

--- inventory.gd
+++ inventory.gd
@@ -179,17 +179,13 @@ func place_mask():
 	mask_held.global_position = get_global_mouse_position()
 	
 	var masked_items = []
-	var grid_ids = []
 	for grid in mask_held.mask_grids:
 		var grid_to_check = current_slot.slot_ID + grid[0] + grid[1] * col_count
 		if grid_array[grid_to_check].States.TAKEN:
 			if grid_array[grid_to_check].cooldown_timer.cooldown == 0:
 				if grid_array[grid_to_check].item_stored:
-					#print("ITEM STORED ID: " + str(grid_array[grid_to_check].item_stored.load_item()["Armor"]))
 					masked_items.append(grid_array[grid_to_check].item_stored)
-				grid_ids.append(grid_to_check)
 			grid_array[grid_to_check].cooldown_timer.cooldown += cooldown_per_activation
-			print(grid_array[grid_to_check].cooldown_timer.cooldown)
 	
 	mask_held = null
 	
